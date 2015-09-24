d746d101e8708f42d07c0e3901646039d2e25e9b
lizhenfeng1985 <lizhenfeng@godinsec.com>
2015-09-24 14:07:55 +0800

    bat

--- NaseConsoleCenter/install.bat
+++ NaseConsoleCenter/install.bat
@@ -1,15 +1,16 @@
-"C:\\Program Files\\NaseForXP\\NaseConsoleCenter\\instsrv.exe"  NaseForXPConsoleCenter "C:\\Program Files\\NaseForXP\\NaseConsoleCenter\\srvany.exe"
+@echo off
+@"C:\\Program Files\\NaseForXP\\NaseConsoleCenter\\instsrv.exe"  NaseForXPConsoleCenter "C:\\Program Files\\NaseForXP\\NaseConsoleCenter\\srvany.exe"
 
-reg add HKLM\SYSTEM\CurrentControlSet\Services\NaseForXPConsoleCenter\Parameters /f
+@reg add HKLM\SYSTEM\CurrentControlSet\Services\NaseForXPConsoleCenter\Parameters /f
 
-reg add HKLM\SYSTEM\CurrentControlSet\Services\NaseForXPConsoleCenter\Parameters /v "Application"  /t REG_SZ /d "C:\\Program Files\\NaseForXP\\NaseConsoleCenter\\NaseConsoleCenter.exe"
+@reg add HKLM\SYSTEM\CurrentControlSet\Services\NaseForXPConsoleCenter\Parameters /v "Application"  /t REG_SZ /d "C:\\Program Files\\NaseForXP\\NaseConsoleCenter\\NaseConsoleCenter.exe"
 
-reg add HKLM\SYSTEM\CurrentControlSet\Services\NaseForXPConsoleCenter\Parameters /v "AppDirectory"  /t REG_SZ /d "C:\\Program Files\\NaseForXP\\NaseConsoleCenter"
+@reg add HKLM\SYSTEM\CurrentControlSet\Services\NaseForXPConsoleCenter\Parameters /v "AppDirectory"  /t REG_SZ /d "C:\\Program Files\\NaseForXP\\NaseConsoleCenter"
 
-reg add HKLM\SYSTEM\CurrentControlSet\Services\NaseForXPConsoleCenter\Parameters /v "AppParameters"  /t REG_SZ /d ""
+@reg add HKLM\SYSTEM\CurrentControlSet\Services\NaseForXPConsoleCenter\Parameters /v "AppParameters"  /t REG_SZ /d ""
 
-sc config NaseForXPConsoleCenter start= auto
+@sc config NaseForXPConsoleCenter start= auto
 
-net start NaseForXPConsoleCenter
+@net start NaseForXPConsoleCenter
 
 
