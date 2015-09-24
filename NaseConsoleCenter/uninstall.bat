08dec3ef96eaa8e29d67c7e16226a7bd84531cd4
lizhenfeng1985 <lizhenfeng@godinsec.com>
2015-09-24 14:26:49 +0800

    bat

--- NaseConsoleCenter/uninstall.bat
+++ NaseConsoleCenter/uninstall.bat
@@ -1,13 +1,13 @@
 @echo off
 
-cd %ProgramFiles%\\NaseForXP\\NaseConsoleCenter
+@cd %ProgramFiles%\\NaseForXP\\NaseConsoleCenter
 
-taskkill /F /im NaseConsoleCenter
-taskkill /F /im NASE管理中心.exe
+@taskkill /F /im NaseConsoleCenter
+@taskkill /F /im NASE管理中心.exe
 
 @net stop NaseForXPConsoleCenter
-instsrv.exe NaseForXPConsoleCenter remove
+@instsrv.exe NaseForXPConsoleCenter remove
 
-rd /S /Q .
-del %0
-echo "卸载成功"
+@rd /S /Q .
+@del %0
+@echo "卸载成功"
